71e77aaad6f2cae67073f8ea182587c58a196d56
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-04-25 22:42:38 +0600

    some new things added

--- js/dom.js
+++ js/dom.js
@@ -2,4 +2,6 @@ let sections = document.querySelectorAll('section');
 for (let section of sections) {
     section.style.border = '2px solid green';
     section.style.marginBottom = ' 10px';
+    section.style.borderRadius = '12px';
+    section.style.backgroundColor = 'purple';
 }
