f545a204e6760e9fc096651a8fa637561bde8071
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-04-26 17:14:28 +0600

    append js added

---
+++ js/append.js
@@ -0,0 +1,4 @@
+let placeList = document.getElementById('fruit-list');
+let add = document.createElement('li');
+add.innerText = 'Palm';
+placeList.appendChild(add);
--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,6 @@
-let sections = document.querySelectorAll('section');
-for (let section of sections) {
-    section.style.border = '2px solid green';
+let title = document.querySelectorAll('h1');
+for (let title of h1) {
+    title.style.border = '2px solid green';
     section.style.marginBottom = ' 10px';
     section.style.borderRadius = '12px';
     section.style.backgroundColor = 'purple';
